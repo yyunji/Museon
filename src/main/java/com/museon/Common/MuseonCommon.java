109fd1425359e9bcd03ce728d3716681a7be5ec7
SongChic <song_chic@naver.com>
2016-07-08 20:12:31 +0900

    google test

---
+++ src/main/java/com/museon/Common/MuseonCommon.java
@@ -0,0 +1,8 @@
+package com.museon.Common;
+
+public class MuseonCommon {
+	public static final String appName = "MuseOn";
+	
+	public static final String googleApiKey = "993076257216-5nj8hufr58q2cmrtgjv8n4lu4gqssdm3.apps.googleusercontent.com";
+	public static final String googleApiPw = "0l4tCWlTonyiUbh1mFWdo8-2";
+}
